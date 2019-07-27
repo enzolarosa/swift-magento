d4b7e35fe3960f9950212c4b1303518cf9c600ba
Vincenzo <hello@enzolarosa.dev>
2019-07-27 09:20:24 +0200

    Initial Commit

---
+++ MagentoSDK/MagentoSDK.swift
@@ -0,0 +1,11 @@
+//
+//  MagentoSDK.swift
+//  MagentoSDK
+//
+//  Created by Vincenzo La Rosa on 27/07/2019.
+//  Copyright © 2019 Vincenzo La Rosa. All rights reserved.
+//
+
+class MagentoSDK {
+
+}
