34fd6ebb2dce54d9761eaaa33bf673f7b22e0f14
Vincenzo <hello@enzolarosa.dev>
2019-07-30 20:58:47 +0200

    Added Test Application

---
+++ MagentoSDK Test/MagentoSDK Test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MagentoSDK Test
+//
+//  Created by Vincenzo La Rosa on 30/07/2019.
+//  Copyright © 2019 Vincenzo La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let username:String;
+    let password:String;
+    let logs:String;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btn_login(_ sender: Any) {
+        
+    }
+    
+}
---
+++ MagentoSDK Test/MagentoSDK TestTests/MagentoSDK_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MagentoSDK_TestTests.swift
+//  MagentoSDK TestTests
+//
+//  Created by Vincenzo La Rosa on 30/07/2019.
+//  Copyright © 2019 Vincenzo La Rosa. All rights reserved.
+//
+
+import XCTest
+@testable import MagentoSDK_Test
+
+class MagentoSDK_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
